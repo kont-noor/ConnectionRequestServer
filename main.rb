1a7905919702de59ace4f090c9dbad1678484c74
Nickolay <devmarkup@gmail.com>
2012-05-15 17:39:31 +0300

    template of connection response

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
+require 'yaml'
 
 class ConnectionRequestServer < Sinatra::Base
 
   post '/request_permission_to_connect' do
-    request_permission_to_connect params
+    response_code = request_permission_to_connect params
+    content_type 'text/xml'
+
+    message =  get_messages
+    erb :connection_response, :locals => {:code => response_code, :message => message[response_code]}
   end
 
   post '/disconnect' do
@@ -20,26 +25,23 @@ class ConnectionRequestServer < Sinatra::Base
     '404'
   end
 
+  def get_messages
+    messages_yaml = File.read(File.dirname(__FILE__) + '/config/messages.yml')
+    YAML.load(messages_yaml)
+  end
+
   def request_permission_to_connect param
     activation_code = param[:activation_code]
     device_id = param[:device_id]
     client_version = param[:client_version]
     os_version = param[:os_version]
 
-    response = {
-      1 =>'Approved',
-      400 => 'Sorry, your account is currently connected from another computer. You can use our service from multiple computers, but each account can only be connected to our network from one computer at a time. To connect from this computer now, please buy an additional account.',
-      401 => "Missing parameters. Sorry, we've made a note to fix this. Please try again and contact support if you continue to see this error.",
-      500 => 'Sorry, unknown error. Please try again and contact support if you continue to see this error.'
-    }
-  
     if (activation_code.nil? or device_id.nil?)
-      response_code = 401
+      return 401
     else
-      response_code = 500
+      return 500
     end
 
-    "<connection_request_response><code>#{response_code}</code><message>#{response[response_code]}</message></connection_request_response>"
   end
 
   def disconnect param
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -32,6 +32,16 @@ describe 'base response' do
 
   it "should response on connect request" do
     post '/request_permission_to_connect'
-    last_response.body.should =~ /\<connection_request_response\>\<code\>.*?\<\/code\>\<message\>.*?\<\/message\>\<\/connection_request_response\>/ims
+    last_response.should be_ok
+  end
+
+  it "should be an xml" do
+    post '/request_permission_to_connect'
+    last_response.header['Content-Type'].should =~ /text\/xml/
+  end
+  
+  it "should be a standard connection response" do
+    post '/request_permission_to_connect'
+    last_response.body.should =~ /\<connection_request_response\>\s*\<code\>.*?\<\/code\>\s*\<message\>.*?\<\/message\>\s*\<\/connection_request_response\>/ims
   end
 end
