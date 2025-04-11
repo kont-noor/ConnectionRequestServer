d0a1fcc75101b5a17fc0f60dd5b79a44139f38a9
Nickolay Kondratenko <devmarkup@gmail.com>
2025-04-11 12:37:15 +0300

    test heartbeat; test disconnect

--- internal/service/mocks_test.go
+++ internal/service/mocks_test.go
@@ -1,6 +1,10 @@
 package service
 
-import "connection_request_server/internal/domain"
+import (
+	"connection_request_server/internal/domain"
+	"errors"
+	"time"
+)
 
 type mockRepository struct {
 	Connections []*domain.Connection
@@ -27,9 +31,41 @@ func (t *mockRepository) InsertConnection(connection *domain.Connection) error {
 }
 
 func (t *mockRepository) DeleteConnection(userID, deviceID string) error {
-	return nil
+	if t.Error != nil {
+		return t.Error
+	}
+
+	for i, connection := range t.Connections {
+		if connection.UserID == userID && connection.DeviceID == deviceID {
+			t.deleteConnAt(i)
+			return nil
+		}
+	}
+
+	return errors.New("connection not found")
 }
 
 func (t *mockRepository) HeartbeatConnection(userID, deviceID string) error {
-	return nil
+	if t.Error != nil {
+		return t.Error
+	}
+
+	for _, connection := range t.Connections {
+		if connection.UserID == userID && connection.DeviceID == deviceID {
+			connection.LastHeartbeat = time.Now()
+			return nil
+		}
+	}
+
+	return errors.New("connection not found")
+}
+
+func (t *mockRepository) deleteConnAt(index int) {
+	if len(t.Connections) == 1 && index == 0 {
+		t.Connections = []*domain.Connection{}
+		return
+	}
+
+	t.Connections[index] = t.Connections[len(t.Connections)-1]
+	t.Connections = t.Connections[:len(t.Connections)-1]
 }
--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -46,7 +47,7 @@ func TestConnect(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			service, repo := createMockService()
-			req := createRequest(http.MethodGet, "/connect", test.input)
+			req := createRequest(http.MethodPost, "/connect", test.input)
 			res := httptest.NewRecorder()
 
 			service.Connect(res, req)
@@ -56,6 +57,97 @@ func TestConnect(t *testing.T) {
 
 			if test.validate != nil {
 				test.validate(t, repo)
+			}
+		})
+	}
+}
+
+func TestDisconnect(t *testing.T) {
+	tests := []struct {
+		name           string
+		input          *requestParams
+		expectedStatus int
+		expectedConns  int
+		validate       func(*testing.T, *mockRepository)
+	}{
+		{
+			name:           "Bad Request - no body",
+			input:          nil,
+			expectedStatus: http.StatusBadRequest,
+			expectedConns:  1,
+		},
+		{
+			name:           "Connection does not exist",
+			input:          &requestParams{UserID: "User1", DeviceID: "Device1"},
+			expectedStatus: http.StatusInternalServerError,
+			expectedConns:  1,
+		},
+		{
+			name:           "Delete connection",
+			input:          &requestParams{UserID: "USER_ID", DeviceID: "DEVICE_ID"},
+			expectedStatus: http.StatusOK,
+			expectedConns:  0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			service, repo := createMockService()
+			req := createRequest(http.MethodDelete, "/disconnect", test.input)
+			res := httptest.NewRecorder()
+
+			service.Disconnect(res, req)
+
+			assert.Equal(t, test.expectedStatus, res.Code)
+			assert.Len(t, repo.Connections, test.expectedConns)
+
+			if test.validate != nil {
+				test.validate(t, repo)
+			}
+		})
+	}
+}
+
+func TestHeartbeat(t *testing.T) {
+	tests := []struct {
+		name             string
+		input            *requestParams
+		expectedStatus   int
+		heartbeatUpdated bool
+	}{
+		{
+			name:             "Bad Request - no body",
+			input:            nil,
+			expectedStatus:   http.StatusBadRequest,
+			heartbeatUpdated: false,
+		},
+		{
+			name:             "Connection does not exist",
+			input:            &requestParams{UserID: "User1", DeviceID: "Device1"},
+			expectedStatus:   http.StatusInternalServerError,
+			heartbeatUpdated: false,
+		},
+		{
+			name:             "Update heartbeat",
+			input:            &requestParams{UserID: "USER_ID", DeviceID: "DEVICE_ID"},
+			expectedStatus:   http.StatusOK,
+			heartbeatUpdated: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			service, repo := createMockService()
+			req := createRequest(http.MethodPut, "/heartbeat", test.input)
+			res := httptest.NewRecorder()
+
+			service.Heartbeat(res, req)
+
+			assert.Equal(t, test.expectedStatus, res.Code)
+			if test.heartbeatUpdated {
+				assert.NotEqual(t, time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC), repo.Connections[0].LastHeartbeat)
+			} else {
+				assert.Equal(t, time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC), repo.Connections[0].LastHeartbeat)
 			}
 		})
 	}
