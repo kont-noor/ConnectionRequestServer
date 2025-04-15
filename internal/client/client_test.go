a53b962bc7ee2f3e13f229b7d0e5e543ba4ba079
Nickolay Kondratenko <devmarkup@gmail.com>
2025-04-15 16:06:31 +0300

    make connect tests more readable

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -11,59 +11,59 @@ import (
 )
 
 func TestConnect(t *testing.T) {
-	tests := []struct {
-		Name             string
-		ReceivedResponse int
-		IsError          bool
-		Heartbeat        bool
-	}{
-		{
-			Name:             "Server returns 500",
-			ReceivedResponse: http.StatusInternalServerError,
-			IsError:          true,
-			Heartbeat:        false,
-		},
-		{
-			Name:             "Connection already exists",
-			ReceivedResponse: http.StatusConflict,
-			IsError:          true,
-			Heartbeat:        false,
-		},
-		{
-			Name:             "Connected successfully",
-			ReceivedResponse: http.StatusOK,
-			IsError:          false,
-			Heartbeat:        true,
-		},
-	}
+	t.Run("Successful connect", func(t *testing.T) {
+		t.Parallel()
 
-	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
-			t.Parallel()
+		server := newMockServer(http.StatusOK, http.StatusOK)
+		defer server.Server.Close()
 
-			server := newMockServer(test.ReceivedResponse, http.StatusOK)
-			defer server.Server.Close()
+		client := newClient(server.Server.URL)
 
-			client := newClient(server.Server.URL)
+		err := client.Connect()
+		assert.NoError(t, err)
 
-			err := client.Connect()
-			if test.IsError {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-			}
+		time.Sleep(2 * time.Second)
 
-			time.Sleep(2 * time.Second)
+		server.mu.Lock()
+		assert.GreaterOrEqual(t, server.HeartbeatCount, 1)
+		server.mu.Unlock()
+	})
 
-			server.mu.Lock()
-			if test.Heartbeat {
-				assert.GreaterOrEqual(t, server.HeartbeatCount, 1)
-			} else {
-				assert.Zero(t, server.HeartbeatCount)
-			}
-			server.mu.Unlock()
-		})
-	}
+	t.Run("Connection exists", func(t *testing.T) {
+		t.Parallel()
+
+		server := newMockServer(http.StatusConflict, http.StatusOK)
+		defer server.Server.Close()
+
+		client := newClient(server.Server.URL)
+
+		err := client.Connect()
+		assert.Error(t, err)
+
+		time.Sleep(2 * time.Second)
+
+		server.mu.Lock()
+		assert.Zero(t, server.HeartbeatCount)
+		server.mu.Unlock()
+	})
+
+	t.Run("Internal server error", func(t *testing.T) {
+		t.Parallel()
+
+		server := newMockServer(http.StatusInternalServerError, http.StatusOK)
+		defer server.Server.Close()
+
+		client := newClient(server.Server.URL)
+
+		err := client.Connect()
+		assert.Error(t, err)
+
+		time.Sleep(2 * time.Second)
+
+		server.mu.Lock()
+		assert.Zero(t, server.HeartbeatCount)
+		server.mu.Unlock()
+	})
 }
 
 func TestDisconnect(t *testing.T) {
